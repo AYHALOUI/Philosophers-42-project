faf3b9e3511f5c851f43dba58688bcc7e0592a5b
aymene haloui <aymenhaloui00@gmail.com>
2023-06-23 02:28:52 +0100

    fixed some errors

---
+++ philo/philo.h
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaloui <ahaloui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/17 23:33:06 by ahaloui           #+#    #+#             */
+/*   Updated: 2023/06/22 21:33:13 by ahaloui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# define KNRM	"\x1B[0m"
+# define END   "\e[0m"
+# define KRED  "\x1B[31m"
+# define KGRN  "\x1B[32m"
+# define KYEL  "\x1B[33m"
+# define KBLU  "\x1B[34m"
+# define KMAG  "\x1B[35m"
+# define KCYN  "\x1B[36m"
+# define KWHT  "\x1B[37m"
+
+# include <pthread.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct s_data
+{
+	int				num_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_of_times_each_philo_must_eat;
+	int				start_time;
+	int				flag;
+	pthread_mutex_t	id_mutex;
+	pthread_mutex_t	start_time_mutex;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	last_meal_mutex;
+}	t_data;
+
+typedef struct s_philo
+{
+	int				id;
+	int				last_meal_time;
+	int				eat_count;
+	t_data			*data;
+	pthread_t		thread;
+	pthread_mutex_t	fork_mutex;
+	struct s_philo	*next;
+}	t_philo;
+
+int		is_white_space(char c);
+int		ft_atoi(char *str);
+int		ft_isdigit(int c);
+int		check_if_valid_args(int ac, char **av);
+void	init_data(t_data *data, int ac, char **av);
+t_philo	*ft_lstnew(t_philo *philo, int id, t_philo *tmp, t_data *data);
+int		my_gettime(void);
+void	my_printf(t_philo *philo, char *str, char *color);
+int		my_usleep(int time);
+void	add_to_table(t_philo **philos, t_data *data);
+void	philosopher_thread(t_philo *philo);
+void	print_philo(t_philo *philos);
+void	help_create_threads(t_philo *philos);
+void	philosopher_thread(t_philo *philo);
+void	destroy_mutex(t_philo *philo);
+void	monitor_threads(t_philo *philos);
+
+#endif
---
+++ philo/utils1.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils1.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaloui <ahaloui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/18 01:56:17 by ahaloui           #+#    #+#             */
+/*   Updated: 2023/06/22 21:33:38 by ahaloui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	init_data(t_data *data, int ac, char **av)
+{
+	data->num_of_philo = ft_atoi(av[1]);
+	data->time_to_die = ft_atoi(av[2]);
+	data->time_to_eat = ft_atoi(av[3]);
+	data->time_to_sleep = ft_atoi(av[4]);
+	data->start_time = my_gettime();
+	data->flag = 0;
+	pthread_mutex_init(&data->last_meal_mutex, NULL);
+	pthread_mutex_init(&data->start_time_mutex, NULL);
+	pthread_mutex_init(&data->print_mutex, NULL);
+	if (ac == 6)
+		data->num_of_times_each_philo_must_eat = ft_atoi(av[5]);
+	else
+		data->num_of_times_each_philo_must_eat = 0;
+}
+
+t_philo	*ft_lstnew(t_philo *philo, int id, t_philo *tmp, t_data *data)
+{
+	t_philo	*new;
+
+	(void)philo;
+	new = (t_philo *)malloc(sizeof(t_philo));
+	if (!new)
+		return (NULL);
+	new->id = id;
+	new->data = data;
+	new->last_meal_time = my_gettime();
+	new->next = tmp;
+	return (new);
+}
